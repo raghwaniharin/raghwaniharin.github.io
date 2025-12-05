b40a489ab0283b07fa14c665686a2fbce0bd226e
raghwaniharin <73600556+raghwaniharin@users.noreply.github.com>
2025-12-05 13:36:27 +1300

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './Styles/style1.css';
-import App from './components/App'; // Import your main App component
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />); // Render the full App
